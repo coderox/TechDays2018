74fcde5856a42a25385a7a8387256abb12b60fed
Johan Lindfors <johan.lindfors@coderox.se>
2018-10-24 15:50:37 +0200

    Initial solution

---
+++ XamlIslands.WPF/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace XamlIslands.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Windows.UI.Xaml.Controls.Button uwpButton;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnChildChanged(object sender, EventArgs e)
+        {
+            if (uwpButton == null)
+            {
+                uwpButton = ((Microsoft.Toolkit.Wpf.UI.XamlHost.WindowsXamlHost)sender).Child as Windows.UI.Xaml.Controls.Button;
+                uwpButton.HorizontalAlignment = Windows.UI.Xaml.HorizontalAlignment.Stretch;
+                uwpButton.VerticalAlignment = Windows.UI.Xaml.VerticalAlignment.Stretch;
+                uwpButton.Background = new Windows.UI.Xaml.Media.SolidColorBrush(Windows.UI.Colors.Red);
+                uwpButton.Click += delegate { uwpButton.Background = new Windows.UI.Xaml.Media.SolidColorBrush(Windows.UI.Colors.Blue); };
+            }
+        }
+    }
+}
---
+++ XamlIslands.WinForms/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows.Forms;
+
+using Microsoft.Toolkit.Forms.UI.XamlHost;
+using Microsoft.Toolkit.Win32.UI.XamlHost;
+
+namespace XamlIslands.WinForms
+{
+    public partial class Form1 : Form
+    {
+        Windows.UI.Xaml.Controls.Button uwpButton;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            UseHelperMethod();
+        }
+
+        private void UseHelperMethod()
+        {
+            var myHostControl = new WindowsXamlHost();
+            myHostControl.Dock = DockStyle.Fill;
+
+            // Use helper method to create a UWP control instance.
+            uwpButton =
+                UWPTypeFactory.CreateXamlContentByType("Windows.UI.Xaml.Controls.Button")
+                as Windows.UI.Xaml.Controls.Button;
+
+            // Initialize UWP control.
+            uwpButton.Name = "button1";
+            uwpButton.HorizontalAlignment = Windows.UI.Xaml.HorizontalAlignment.Stretch;
+            uwpButton.VerticalAlignment = Windows.UI.Xaml.VerticalAlignment.Stretch;
+            uwpButton.Background = new Windows.UI.Xaml.Media.SolidColorBrush(Windows.UI.Colors.Red);
+            uwpButton.Click += delegate { uwpButton.Background = new Windows.UI.Xaml.Media.SolidColorBrush(Windows.UI.Colors.Blue); };
+
+            // initialize the Windows XAML host control.
+            myHostControl.Name = "myWindowsXamlHostControl";
+            myHostControl.Child = uwpButton;
+
+            // Make the UWP control appear in the UI.
+            this.Controls.Add(myHostControl);
+        }
+    }
+}
